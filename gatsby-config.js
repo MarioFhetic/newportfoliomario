afcef09b48109ee31bd54a85cb6dfcc48df5948a
MarioFhetic <fayollemario@outlook.fr>
2020-10-31 15:36:54 +0100

    new build

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,8 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.API_URL || "http://localhost:1337",
+        // apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`projects`],
         //If using single types place them in this array.
